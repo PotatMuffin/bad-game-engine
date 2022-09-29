d4277466af03af86645b8e468cbd76fa7e864bf9
PotatMuffin <113198494+PotatMuffin@users.noreply.github.com>
2022-09-29 13:43:24 +0200

    Add files via upload

--- engine/game_object.py
+++ engine/game_object.py
@@ -1,4 +1,3 @@
-import future
 from .vector import Vector
 from uuid import uuid4
 
@@ -7,6 +6,10 @@ OVAL = 'oval'
 ARC = 'arc'
 
 class Transform:
+    """
+    the Transform class that holds the position, width, and height of a Game Object
+    """
+
     def __init__(self, x:float, y:float, width:float, height:float):
         self.__position = Vector(x, y)
         self.__width = width
@@ -16,13 +19,25 @@ class Transform:
         return f"position: {self.__position}, width: {self.__width}, height: {self.__height}"
 
     def set_position(self, x:float=None, y:float=None):
+        """
+        changes position 
+        """
+
         self.__position = Vector(x or self.__position.x, y or self.__position.y)
     
     def set_dimensions(self, width:float=None, height:float=None):
+        """
+        changes the width and/or height 
+        """
+
         self.__width = width or self.__width
         self.__height = height or self.__height
 
     def move_towards(self, speed: float, position: Vector):
+        """
+        moves the Game Object that holds the Transform to a a specified positon at the given speed 
+        """
+
         if self.position.x > position.x:
             x = self.position.x - speed
             if x < position.x:
@@ -60,6 +75,10 @@ class Transform:
         return Vector(self.__position.x + self.__width / 2, self.__position.y + self.__height / 2)
 
 class Game_Object:
+    """
+    basic Game Object that is part of a scene
+    """
+
     def __init__(self, transform:Transform, tags:list[str]=None, name:str=None, shape=RECT, colour:str="#000000"):
         self.__transform = transform
         self.__colour = colour
